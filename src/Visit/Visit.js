94ee89af8b2ee241d713a2921b1ab8ab5cf1764d
szopszop <szymontracz1@gmail.com>
2022-11-10 15:17:13 +0100

    single visit

--- src/Visit/Visit.js
+++ src/Visit/Visit.js
@@ -4,6 +4,7 @@ import 'bootstrap/dist/css/bootstrap.css';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 import {faCalendarDays, faLocationDot, faChartLine, faPills, faXmark, faVideo} from '@fortawesome/free-solid-svg-icons'
 import Collapse from 'react-bootstrap/Collapse';
+import {forEach} from "react-bootstrap/ElementChildren";
 
 export default function Visit() {
     // Rozsuwane See Details
@@ -12,168 +13,95 @@ export default function Visit() {
     const PATIENT_ID = "1"
     const listOfVisitsURL = BASE_URL + PATIENT_ID
 
-    const [listOfVisits, setListsOfVisits] = useState("")
+    const [listOfVisits, setListsOfVisits] = useState()
 
-    async function getList(listOfVisitsURL, setOpen, open) {
+    async function getList(listOfVisitsURL) {
         await fetch(listOfVisitsURL)
-            .then(res => JSON.parse(String(res)))
+            .then(res => res.json())
             .then((res) => {
-
+                setListsOfVisits(res)
+                console.warn("!")
                 console.log(res)
-                const newListOfVisits = res.map((v) => {
-                    return (
-                        <div className="row align-items-center" key={v.date}>
 
-                        <div className="row align-items-center">
-                            <div className="col-auto my-3 mx-2 container rounded-3 bg-white text-dark shadow-sm">
-                                {v}
-                            </div>
+                res.forEach(v => console.log(v))
 
-                            <div className="col-9 container rounded-3 bg-white text-dark my-3">
-                                <div className="row justify-content-between">
-                                    <div className="col-4 fs-3">
-                                        {v.doctorFirstName.value} {v.doctorLastName.value}
-                                    </div>
-                                    <div className="col-auto text-capitalize text-decoration-underline"
-                                         onClick={() => setOpen(!open)}
-                                         aria-controls="example-collapse-text"
-                                         aria-expanded={open}>
-                                        See details
-                                    </div>
-                                </div>
-
-                                <div className="row align-items-center">
-                                    <div className="col-2">
-                                        <img src="https://www.getmaple.ca/site-content/uploads/2020/08/female_dr.jpg"
-                                             className="img-fluid rounded-circle"
-                                             style={{height: "100px", width: "100px"}}
-                                             alt="doctor"/>
-                                    </div>
-                                    <div className="col-5 m-1">
-                                        <div className="row fs-5 mx-1">
-                                            {v.doctorSpecialities.value}
-                                        </div>
-                                        <div className="row ">
-                                            <div className="col text-primary ">
-                                                <FontAwesomeIcon icon={faLocationDot} className="me-2"/>
-                                                {v.clinicName.value}, X: {v.longitude.value}, Y: {v.latitude.value}
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                                {/* Rozsuwane*/}
-
-                                <Collapse in={open}>
-                                    <div>
-                                        <br/>
-                                        <hr/>
-                                        <br/>
-                                        <div id="example-collapse-text">
-                                            <div className="fs-4">
-                                                Recommendations
-                                            </div>
-                                            <div>
-                                                first name
-                                                <br/>
-                                                {/*{v.patient.birthDate}*/}
-                                            </div>
-                                            <br/>
-                                            <hr/>
-                                            <br/>
-                                            <div className="fs-4">
-                                                Referrals
-                                            </div>
-                                            <div>
-                                                {/*{v.patient.password}*/}
-                                                <br/>
-                                                {/*{v.patient.medicalData.weight}*/}
-                                            </div>
-                                        </div>
-                                    </div>
-                                </Collapse>
-                            </div>
-                        </div>
-                    </div>)
-                })
-                console.log(newListOfVisits[0]);
-                setListsOfVisits(String(newListOfVisits[0]))
+                // setListsOfVisits(newListOfVisits)
             })
     }
 
-    useEffect(() => {
-            getList(listOfVisitsURL, setOpen, open)
-                .then(() => {
-                });
-        }
-    )
+        useEffect(() => {
+                getList(listOfVisitsURL).then(r => console.log("RRRRRR" + r))
 
-    return (
-        <React.Fragment>
+            }
+        )
 
-            <ChakraProvider>
-                <div className="container col-6 mx-auto rounded-5 bg-dark text-dark bg-opacity-10 shadow">
+        return (
+            <React.Fragment>
 
-                    <div className="row justify-content-center">
-                        <div className="col-auto my-3 mx-2 container rounded-3 bg-white text-dark shadow-sm">
-                            Today's visits:
+                <ChakraProvider>
+                    <div className="container col-6 mx-auto rounded-5 bg-dark text-dark bg-opacity-10 shadow">
+
+                        <div className="row justify-content-center">
+                            <div className="col-auto my-3 mx-2 container rounded-3 bg-white text-dark shadow-sm">
+                                Today's visits:
+                            </div>
+                            <div className="col-9 container rounded-3 bg-white text-dark my-3 shadow-sm">
+                                You do not have appointments planned for today.
+                            </div>
                         </div>
-                        <div className="col-9 container rounded-3 bg-white text-dark my-3 shadow-sm">
-                            You do not have appointments planned for today.
+
+                        <br/>
+
+                        <div className="row justify-content-center">
+                            <div className="col-2"></div>
+                            <div className="col fs-3 ">History</div>
                         </div>
+
+                        <br/>
+
+                        <div className="row ">
+                            <div className="col-2">
+
+                            </div>
+                            <div className="col-auto rounded-5 bg-white m-2 shadow-sm">
+                                <FontAwesomeIcon icon={faCalendarDays} className="me-2"/>
+                                Data
+                            </div>
+                            <div className="col-auto rounded-5 bg-white m-2">
+                                <FontAwesomeIcon icon={faLocationDot} className="me-2"/>
+                                Visits
+                            </div>
+                            <div className="col-auto rounded-5 bg-white m-2">
+                                <FontAwesomeIcon icon={faChartLine} className="me-2"/>
+                                Examinations
+                            </div>
+                            <div className="col-auto rounded-5 bg-white m-2">
+                                <FontAwesomeIcon icon={faPills} className="me-2"/>
+                                Prescriptions
+                            </div>
+                            <div className="col-auto rounded-5 bg-white m-2">
+                                <FontAwesomeIcon icon={faVideo} className="me-2"/>
+                                Video calls
+                            </div>
+                            <div className="col-auto m-2">
+                                <FontAwesomeIcon icon={faXmark} className="me-2"/>
+                                Clear
+                            </div>
+                        </div>
+
+                        {listOfVisits}
+
                     </div>
+
 
                     <br/>
-
-                    <div className="row justify-content-center">
-                        <div className="col-2"></div>
-                        <div className="col fs-3 ">History</div>
-                    </div>
-
+                    <br/>
+                    <br/>
                     <br/>
 
-                    <div className="row ">
-                        <div className="col-2">
 
-                        </div>
-                        <div className="col-auto rounded-5 bg-white m-2 shadow-sm">
-                            <FontAwesomeIcon icon={faCalendarDays} className="me-2"/>
-                            Data
-                        </div>
-                        <div className="col-auto rounded-5 bg-white m-2">
-                            <FontAwesomeIcon icon={faLocationDot} className="me-2"/>
-                            Visits
-                        </div>
-                        <div className="col-auto rounded-5 bg-white m-2">
-                            <FontAwesomeIcon icon={faChartLine} className="me-2"/>
-                            Examinations
-                        </div>
-                        <div className="col-auto rounded-5 bg-white m-2">
-                            <FontAwesomeIcon icon={faPills} className="me-2"/>
-                            Prescriptions
-                        </div>
-                        <div className="col-auto rounded-5 bg-white m-2">
-                            <FontAwesomeIcon icon={faVideo} className="me-2"/>
-                            Video calls
-                        </div>
-                        <div className="col-auto m-2">
-                            <FontAwesomeIcon icon={faXmark} className="me-2"/>
-                            Clear
-                        </div>
-                    </div>
+                </ChakraProvider>
+            </React.Fragment>
 
-                    {listOfVisits}
-
-                </div>
-
-
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-
-
-            </ChakraProvider>
-        </React.Fragment>
-
-    );
-}
+        );
+    }
