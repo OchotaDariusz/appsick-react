4fc759254f02c433eea3884b78e03fd98ec75cda
szopszop <szymontracz1@gmail.com>
2022-11-14 18:09:14 +0100

    refactored time filter function

--- src/Visit/Visit.js
+++ src/Visit/Visit.js
@@ -9,10 +9,10 @@ export default function Visit({ visit }) {
   return (
     <div className="row align-items-center" >
 
-      <div className="row align-items-center">
-        <div className="col-auto my-3 mx-2 container rounded-3 bg-white text-dark shadow-sm">
-          {visit.date}
-        </div>
+            <div className="row align-items-center">
+                <div className="col-auto my-3 mx-2 container rounded-3 bg-white text-dark shadow-sm p-2">
+                    {visit.date}
+                </div>
 
         <div className="col-9 container rounded-3 bg-white text-dark my-3">
           <div className="row justify-content-between">
@@ -27,25 +27,25 @@ export default function Visit({ visit }) {
             </div>
           </div>
 
-          <div className="row align-items-center">
-            <div className="col-2">
-              <img src="https://www.getmaple.ca/site-content/uploads/2020/08/female_dr.jpg"
-                   className="img-fluid rounded-circle"
-                   style={{height: "100px", width: "100px"}}
-                   alt="doctor"/>
-            </div>
-            <div className="col-5 m-1">
-              <div className="row fs-5 mx-1">
-                {visit.doctor.medicalSpecialities}
-              </div>
-              <div className="row ">
-                <div className="col text-primary ">
-                  <FontAwesomeIcon icon={faLocationDot} className="me-2"/>
-                  {visit.clinic.clinicName}, X: {visit.clinic.longitude}, Y: {visit.clinic.latitude}
-                </div>
-              </div>
-            </div>
-          </div>
+                    <div className="row align-items-center">
+                        <div className="col-2">
+                            <img src="https://www.getmaple.ca/site-content/uploads/2020/08/female_dr.jpg"
+                                 className="img-fluid rounded-circle"
+                                 style={{height: "100px", width: "100px"}}
+                                 alt="doctor"/>
+                        </div>
+                        <div className="col-5 m-1">
+                            <div className="row fs-5 mx-1">
+                                {finalResult}
+                            </div>
+                            <div className="row ">
+                                <div className="col text-primary ">
+                                    <FontAwesomeIcon icon={faLocationDot} className="me-2"/>
+                                    {visit.clinic.clinicName}, X: {visit.clinic.longitude}, Y: {visit.clinic.latitude}
+                                </div>
+                            </div>
+                        </div>
+                    </div>
 
            {/*Rozsuwane*/}
           <Collapse in={open}>
