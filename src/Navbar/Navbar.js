3d99c517668398800a347eb179e449880bc4d75e
mat-kossowski <mat.kossowski.pl@gmail.com>
2022-11-14 10:51:44 +0100

    upgrade: Home Navbar component upgrade

--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -5,29 +5,20 @@ import logo from './logoAppsick.png'
 export default function Navbar() {
     return (
         <div className="nav">
-            <div className="nav-name">
-                <div className="name"><img src={logo}/></div>
+            <div className={"logo"}>
+                <img src={logo} alt={"logo"}/>
             </div>
-            <div className="nav-list">
+            <div className={"nav-list"}>
                 <ol>
-
                     <li><a href="#">VISIT</a></li>
-                    <li><a href="#">FINDINGS</a></li>
-                    <li><a href="#">DOCTORS</a></li>
                     <li><a href="#">CONTACT</a></li>
+                    <li><a href="#">LOGIN</a></li>
                 </ol>
+
             </div>
-            <div className={"nav-search"}>
-                <div className="search">
-                    <input type="text" className="search__input" aria-label="search"
-                           placeholder="enter your search"/>
-                        <button className="search__submit" aria-label="submit search">
-                            <i className="fa fa-search"></i> </button>
-                </div>
-            </div>
-            <div className={"login-button"}><button className={'btn third'} type={'submit'}>Login</button></div>
+
 
         </div>
 
-);
+    );
 }
