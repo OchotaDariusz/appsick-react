4acfd7b7c02a556bc2b54dc8ee5fb1a40a951911
mat-kossowski <mat.kossowski.pl@gmail.com>
2022-11-15 13:55:25 +0100

    upgrade: Home Navbar component upgrade

--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -1,6 +1,9 @@
 import React from "react"
 import './Navbar.css'
 import logo from './logoAppsick.png'
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import { CiMedicalClipboard } from "react-icons/ci";
+import {faStethoscope} from "@fortawesome/free-solid-svg-icons";
 
 export default function Navbar() {
     return (
@@ -10,8 +13,8 @@ export default function Navbar() {
             </div>
             <div className={"nav-list"}>
                 <ol>
-                    <li><a href="#">VISIT</a></li>
-                    <li><a href="#">CONTACT</a></li>
+                    <li><a href="#"><CiMedicalClipboard className="ml-4 float-start mx-2" size={40}  />VISIT</a></li>
+                    <li><a href="#"><FontAwesomeIcon icon={faStethoscope} className=""/>CONTACT</a></li>
                     <li><a href="#">LOGIN</a></li>
                 </ol>
 
