e04dbbc37da2325f390fe4c4d3834c9d2f0cdd19
Sebastian Dudkowski <sebastian.dudkowski@gmail.com>
2022-11-16 16:56:12 +0100

    define needed hooks and basic structure

--- src/Visit/VisitRegistration.js
+++ src/Visit/VisitRegistration.js
@@ -1,8 +1,7 @@
 import React, { useState, useEffect } from 'react'
 
 const VisitRegistration = () => {
-    const [visitDetails, setVisitDetails] = useState(
-        {
+    let visitObject = {
             "clinic": {
                 "clinicId": 0
             },
@@ -17,30 +16,54 @@ const VisitRegistration = () => {
             "reason": "",
             "status": "PENDING"
         }
-    )
+
+    const [visitDetails, setVisitDetails] = useState(visitObject)
+    const [clinicList, setClinicList] = useState([])
+    const [doctorList, setDoctorList] = useState([])
+    const [visitDateList, setVisitDateList] = useState([])
+
+    const BASE_URL = "http://localhost:8080/api";
+
+    async function getListOfClinics(){
+        const clinics = await fetch(BASE_URL + "/clinic");
+        return clinics.json();
+    }
 
     return (
-        <div>
-
+        <div className="container col-6 mx-auto rounded-5 bg-dark text-dark bg-opacity-10 shadow">
+            <form className="row justify-content-center">
+                <label htmlFor={"clinic"}>Clinic:</label>
+                <select name={"clinic"}>
+                    {}
+                </select>
+                <label htmlFor={"doctor"}>Doctor:</label>
+                <select name={"doctor"}>
+                    {}
+                </select>
+                <label htmlFor={"date"}>Doctor:</label>
+                <select name={"date"}>
+                    {}
+                </select>
+            </form>
         </div>
     );
 };
 
+const clinicOption = (option) => {
+    return (
+        <option>{option}</option>
+    )
+}
+const doctorOption = (option) => {
+    return (
+        <option>{option}</option>
+    )
+}
+const visitDateOption = (option) => {
+    return (
+        <option>{option}</option>
+    )
+}
+
 export default VisitRegistration;
 
-
-// {
-//   "clinic": {
-//     "clinicId": 1
-//   },
-//   "date": "2022-11-14T19:57:07.153Z",
-//   "doctor": {
-//     "doctorId": 1
-//   },
-//   "online": true,
-//   "patient": {
-//     "patientId": 1
-//   },
-//   "reason": "string",
-//   "status": "PENDING"
-// }
