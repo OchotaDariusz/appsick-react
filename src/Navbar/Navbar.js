5226b894d38eb866944f8a698e7d7d9761163151
mat-kossowski <mat.kossowski.pl@gmail.com>
2022-11-17 11:50:52 +0100

    Sticky navbar, navbar collapses to a list

--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -6,62 +6,59 @@ import drugs from './drugstest.png'
 import contact from './contact.png'
 import findings from './findingstest.png'
 import newVisit from './new_visit.png'
-
-
+import {Link} from "react-router-dom";
 
 
 export default function Navbar() {
     return (
-        <div className="    " style={{backgroundColor:'white'}}>
-            <div className="row">
-                <div className="col-3">
-                       <a href={'/'}> <div className="justify-content-center px-5">
-                            <img src={logo} alt="Logo" />
-                        </div>
-                       </a>
-                </div>
-                <div className="col-7 align-items-center">
-                    <div className="row align-items-center" style={{height:'100%'}}>
-                        <div className="col-3 text-justify" >
-                            <a href={'/visit'}>
-                            <img className="float-start w-10 mx-2 " src={visit} alt="Logo" />
-
-                            <p>MY APPOINTMENTS</p>
-                        </a>
-                        </div>
-
-                        <div className="col-2 text-justify" >
-                            <img className="float-start w-10 mx-2" src={findings} alt="Logo" />
-
-                            <p>FINDINGS</p>
-                        </div>
-                        <div className="col-2 text-justify" >
-                            <img className="float-start w-10 mx-2" src={drugs} alt="Logo" />
-
-                            <p>DRUGS</p>
-                        </div>
-                        <div className="col-2 text-justify" >
-                            <a href={'/visit/:visitId'}><img className="float-start w-10 mx-2" src={contact} alt="Logo" />
-
-                            <p>CONTACT</p>
-                        </a>
-                        </div>
-                        <div className="col-3 text-justify" >
-                            <img className="float-start w-10 mx-2" src={newVisit} alt="Logo" />
-
-                            <p>Make an appointment</p>
-                        </div>
+        <nav className="navbar navbar-expand-lg sticky-top bg-light" style={{padding: '0px'}}>
+            <div className="container-fluid">
+                <a className="navbar-brand" href={'/'}>
+                    <div className="justify-content-center px-3">
+                        <img src={logo} alt="Logo"/>
                     </div>
+                </a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText"
+                        aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarText">
+                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li className="nav-item">
+                            <Link className="nav-link" to={'/visit'}>
+                                <img className="float-start mx-2 " src={visit} alt="Logo"/>
+                                <p className={"float-start"}>MY APPOINTMENTS</p></Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to={'#'}><img className="float-start mx-2" src={findings} alt="Logo"/>
+                                <p className={"float-start"}>FINDINGS</p></Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to={'#'}>
+                                <img className="float-start mx-2" src={drugs} alt="Logo"/>
+                                <p className={"float-start"}>DRUGS</p></Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to={'/visit/:visitId'}>
+                                <img className="float-start mx-2" src={contact} alt="Logo"/>
+                                <p className={"float-start"}>CONTACT</p></Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to={'/register-visit'}>
+                                <img className="float-start mx-2" src={newVisit} alt="Logo"/>
+                                <p className={"float-start"}>Make an appointment</p></Link>
+                        </li>
+                    </ul>
+                    <span className="navbar-text">
+                        <Link className="nav-link float-start" to={'/register'}>
+                            <button className="btn btn-secondary btn-sm mx-2 my-1">REGISTRATION</button></Link>
+                        <Link className="nav-link float-start" to={'/login'}>
+                            <button className="btn btn-secondary btn-sm mx-2 my-1">LOGIN</button></Link>
+                    </span>
                 </div>
-                <div className="col-2">
-                    <a href={'/register'}><button className="btn btn-secondary btn-sm my-3 mx-4">REGISTRATION</button></a>
-                    <a href={'/login'}><button className="btn btn-secondary btn-sm">LOGIN</button></a>
-
-                </div>
-
-                {/*<div className={"login-button"}><button className={'btn third'} type={'submit'}>Login</button></div>  Make an appointment */}
             </div>
-        </div>
+        </nav>
 
-    );
+    )
+        ;
 }
