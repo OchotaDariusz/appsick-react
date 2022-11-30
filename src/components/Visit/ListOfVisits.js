8fab8167ce1517d21cb01db21b8ba16d15b9dc55
szopszop <szymontracz1@gmail.com>
2022-11-30 12:58:32 +0100

    pagination on scroll with new and today's visits

--- src/components/Visit/ListOfVisits.js
+++ src/components/Visit/ListOfVisits.js
@@ -74,85 +74,76 @@ export default function ListOfVisits() {
     return (
         <div>
             <div className="container col-6 mx-auto rounded-5 bg-dark text-dark bg-opacity-10 shadow">
+                    <div className="row justify-content-center">
+                        <div className="col-2"></div>
+                        <div className="col fs-3 ">Incoming</div>
+                    </div>
+                    <div className="row align-items-center">
+
+                        {futureVisits.map(visit => <Visit visit={visit} key={visit.visitId}/>)}
+                        <div className="">
+                            <hr/>
+                        </div>
+                        <br/>
+                        <br/>
+                        <div className="row align-items-center">
+                            <div
+                                className="col-auto my-3 mx-2 container bg-white border-2 border-opacity-75 border-dark border rounded text-center">
+                                Today's <br/>visits:
+                            </div>
+
+                            <div className="container col-9">
+                                {currentVisits.length > 0 ?
+                                    currentVisits.map(visit => <TodayVisit visit={visit} key={visit.visitId}/>) :
+                                    "You do not have appointments planned for today."
+                                }
+                            </div>
+                        </div>
+
+                    </div>
+
+                    <br/>
+                    <hr/>
+                    <br/>
+
+                    <div className="row justify-content-center">
+                        <div className="col-2"></div>
+                        <div className="col fs-3 ">History</div>
+                    </div>
+
+                    <br/>
+
+                    <div className="row ">
+                        <div className="col-2">
+
+                        </div>
+                        <div className="col-auto rounded-5 bg-white m-2 shadow-sm">
+                            <FontAwesomeIcon icon={faCalendarDays} className="me-2"/>
+                            Data
+                        </div>
+                        <div className="col-auto rounded-5 bg-white m-2 shadow-sm">
+                            <FontAwesomeIcon icon={faLocationDot} className="me-2"/>
+                            Visits
+                        </div>
+                        <div className="col-auto rounded-5 bg-white m-2 shadow-sm">
+                            <FontAwesomeIcon icon={faChartLine} className="me-2"/>
+                            Examinations
+                        </div>
+                        <div className="col-auto rounded-5 bg-white m-2 shadow-sm">
+                            <FontAwesomeIcon icon={faPills} className="me-2"/>
+                            Prescriptions
+                        </div>
+                        <div className="col-auto rounded-5 bg-white m-2 shadow-sm ">
+                            <FontAwesomeIcon icon={faVideo} className="me-2"/>
+                            Video calls
+                        </div>
+                        <div className="col-auto m-2 shadow-sm">
+                            <FontAwesomeIcon icon={faXmark} className="me-2"/>
+                            Clear
+                        </div>
+                    </div>
+
                 <PastVisits pastVisits={pastVisits} loadMorePastVisits={loadMorePastVisits}/>
-                {/*    <div className="row justify-content-center">*/}
-                {/*        <div className="col-2"></div>*/}
-                {/*        <div className="col fs-3 ">Incoming</div>*/}
-                {/*    </div>*/}
-                {/*    <div className="row align-items-center">*/}
-
-                {/*        {futureVisits.map(visit => <Visit visit={visit} key={visit.visitId}/>)}*/}
-                {/*        <div className="">*/}
-                {/*            <hr/>*/}
-                {/*        </div>*/}
-                {/*        <br/>*/}
-                {/*        <br/>*/}
-                {/*        <div className="row align-items-center">*/}
-                {/*            <div*/}
-                {/*                className="col-auto my-3 mx-2 container bg-white border-2 border-opacity-75 border-dark border rounded text-center">*/}
-                {/*                Today's <br/>visits:*/}
-                {/*            </div>*/}
-
-                {/*            <div className="container col-9">*/}
-                {/*                {currentVisits.length > 0 ?*/}
-                {/*                    currentVisits.map(visit => <TodayVisit visit={visit} key={visit.visitId}/>) :*/}
-                {/*                    "You do not have appointments planned for today."*/}
-                {/*                }*/}
-                {/*            </div>*/}
-                {/*        </div>*/}
-
-                {/*    </div>*/}
-
-                {/*    <br/>*/}
-                {/*    <hr/>*/}
-                {/*    <br/>*/}
-
-                {/*    <div className="row justify-content-center">*/}
-                {/*        <div className="col-2"></div>*/}
-                {/*        <div className="col fs-3 ">History</div>*/}
-                {/*    </div>*/}
-
-                {/*    <br/>*/}
-
-                {/*    <div className="row ">*/}
-                {/*        <div className="col-2">*/}
-
-                {/*        </div>*/}
-                {/*        <div className="col-auto rounded-5 bg-white m-2 shadow-sm">*/}
-                {/*            <FontAwesomeIcon icon={faCalendarDays} className="me-2"/>*/}
-                {/*            Data*/}
-                {/*        </div>*/}
-                {/*        <div className="col-auto rounded-5 bg-white m-2 shadow-sm">*/}
-                {/*            <FontAwesomeIcon icon={faLocationDot} className="me-2"/>*/}
-                {/*            Visits*/}
-                {/*        </div>*/}
-                {/*        <div className="col-auto rounded-5 bg-white m-2 shadow-sm">*/}
-                {/*            <FontAwesomeIcon icon={faChartLine} className="me-2"/>*/}
-                {/*            Examinations*/}
-                {/*        </div>*/}
-                {/*        <div className="col-auto rounded-5 bg-white m-2 shadow-sm">*/}
-                {/*            <FontAwesomeIcon icon={faPills} className="me-2"/>*/}
-                {/*            Prescriptions*/}
-                {/*        </div>*/}
-                {/*        <div className="col-auto rounded-5 bg-white m-2 shadow-sm ">*/}
-                {/*            <FontAwesomeIcon icon={faVideo} className="me-2"/>*/}
-                {/*            Video calls*/}
-                {/*        </div>*/}
-                {/*        <div className="col-auto m-2 shadow-sm">*/}
-                {/*            <FontAwesomeIcon icon={faXmark} className="me-2"/>*/}
-                {/*            Clear*/}
-                {/*        </div>*/}
-                {/*    </div>*/}
-
-                {/*    /!*{pastVisits.map(visit => <Visit visit={visit} key={visit.visitId}/>)}*!/*/}
-                {/*    <List ref={scroller}>*/}
-                {/*        <For data={pastVisits} itemRenderer={(emp, idx) => (*/}
-                {/*            <ListItem key={pastVisits.map(visit => visit.visitId)}>*/}
-
-                {/*                /!*<Visit visit={visit}></Visit>*!/*/}
-                {/*            </ListItem>)}*/}
-                {/*        ></For>*/}
-                {/*    </List>*/}
 
             </div>
 
