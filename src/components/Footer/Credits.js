175e0aa5254c88579ea389161f2d121e03cbf29e
szopszop <szymontracz1@gmail.com>
2022-12-01 22:19:57 +0100

    credits page updated

--- src/components/Footer/Credits.js
+++ src/components/Footer/Credits.js
@@ -7,17 +7,19 @@ export default function Credits() {
             <div
                 className="fs-3 pb-3 container-fluid col-6 mb-5 rounded-5 bg-dark text-dark bg-opacity-10 shadow my-5 text-center">
                 <div>
-                    Thanks to our attributors for images:
+                    Our App wouldn't be the same without the courtesy of the authors <br />
+                    of those wonderful graphics. Special thanks to:
                 </div>
                 <div className="fs-4">
-                    PCH at freepik.com:
+                    PCH from freepik.com:
 
                 </div>
                     <a href="https://www.freepik.com/free-vector/patients-doctors-meeting-waiting-clinic-hall-hospital-
                 interior-illustration-with-reception-person-wheelchair-visiting-doctor-office-medical-examination-
                 consultation_10173282.htm#query=medical%20illustration&position=23&from_view=search&track=sph">
-                        <img src="https://cdn-icons-png.flaticon.com/512/18/18551.png"/>
+                        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/7b/Freepik.svg/2560px-Freepik.svg.png"/>
                     </a>
+
 
             </div>
             <Footer/>
