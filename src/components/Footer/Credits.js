e111bc65574fa01ed9d0815691bdb92f6ed21413
szopszop <szymontracz1@gmail.com>
2022-12-02 20:25:50 +0100

    credits page updated

--- src/components/Footer/Credits.js
+++ src/components/Footer/Credits.js
@@ -4,8 +4,7 @@ import Footer from "./Footer";
 export default function Credits() {
     return (
         <div>
-            <div
-                className="fs-3 pb-3 container-fluid col-6 mb-5 rounded-5 bg-dark text-dark bg-opacity-10 shadow my-5 text-center">
+            <div className="fs-3 pb-3 container-fluid col-6 mb-5 rounded-5 bg-dark text-dark bg-opacity-10 shadow my-5 text-center">
                 <div>
                     Our App wouldn't be the same without the courtesy of the authors <br />
                     of those wonderful graphics. Special thanks to:
@@ -18,6 +17,15 @@ export default function Credits() {
                 interior-illustration-with-reception-person-wheelchair-visiting-doctor-office-medical-examination-
                 consultation_10173282.htm#query=medical%20illustration&position=23&from_view=search&track=sph">
                         <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/7b/Freepik.svg/2560px-Freepik.svg.png"/>
+                    </a>
+
+                <div className="fs-4">
+                    Kampus Production & J Carter from Pexels.com:
+
+
+                </div>
+                    <a href="https://www.pexels.com/pl-pl/zdjecie/zdjecie-starszej-pary-7477717/">
+                        <img src="https://help.pexels.com/hc/en-us/article_attachments/900006864786/Logo_on_Transparent.png"/>
                     </a>
 
 
