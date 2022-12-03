ecf41e52db8eb8613285c0ce4272eb91904070c8
szopszop <szymontracz1@gmail.com>
2022-12-03 15:08:28 +0100

    navbar in the middle with hambuger

--- src/components/Navbar/TheNavbar.js
+++ src/components/Navbar/TheNavbar.js
@@ -15,203 +15,64 @@ import NavDropdown from 'react-bootstrap/NavDropdown';
 export default function TheNavbar() {
     return (
         <>
-            {/*<Navbar collapseOnSelect expand="lg" bg="light" >*/}
-            {/*    <Container>*/}
-
-            {/*        <Navbar.Brand href="/" className="">*/}
-            {/*            <img*/}
-            {/*                alt=""*/}
-            {/*                src={logo}*/}
-            {/*                width="300"*/}
-            {/*                className=""*/}
-            {/*            />{' '}*/}
-            {/*        </Navbar.Brand>*/}
-            {/*        <Navbar.Toggle aria-controls="responsive-navbar-nav" />*/}
-            {/*        <Navbar.Collapse id="responsive-navbar-nav">*/}
-            {/*            <Nav className="container-fluid justify-content-between">*/}
-            {/*                <Nav.Link href="/visit">*/}
-            {/*                    <img*/}
-            {/*                        alt=""*/}
-            {/*                        src={visit}*/}
-            {/*                        width="40"*/}
-            {/*                        className="d-inline-block "*/}
-            {/*                    />*/}
-            {/*                    VISIT HISTORY*/}
-            {/*                </Nav.Link>*/}
-            {/*                <Nav.Link href="/visit">*/}
-            {/*                    <img*/}
-            {/*                        alt=""*/}
-            {/*                        src={drugs}*/}
-            {/*                        width="40"*/}
-            {/*                        className="d-inline-block "*/}
-            {/*                    />DRUGS*/}
-            {/*                </Nav.Link>*/}
-            {/*                <Nav.Link href="/visit">*/}
-            {/*                    <img*/}
-            {/*                        alt=""*/}
-            {/*                        src={contact}*/}
-            {/*                        width="40"*/}
-            {/*                        className="d-inline-block "*/}
-            {/*                    />*/}
-            {/*                    CONTACT</Nav.Link>*/}
-            {/*                <Nav.Link href="/visit">*/}
-            {/*                    <img*/}
-            {/*                        alt=""*/}
-            {/*                        src={newVisit}*/}
-            {/*                        width="40"*/}
-            {/*                        className="d-inline-block "*/}
-            {/*                    />*/}
-            {/*                    MAKE AN<br/> APPOINTMENT</Nav.Link>*/}
-
-            {/*            </Nav>*/}
-            {/*            <Nav>*/}
-            {/*                <div className="d-flex justify-content-end pb-5">*/}
-            {/*                    <div className="button-login  fs-3 text-black border border-dark border-2 rounded-pill p-2 "*/}
-            {/*                         role="button">*/}
-            {/*                        <p>Login | Register </p>*/}
-            {/*                        <div className="fs-1">*/}
-            {/*                            <CgKey/>*/}
-            {/*                            /!*<img src={key} alt="key"/>*!/*/}
-
-            {/*                        </div>*/}
-
-            {/*                    </div>*/}
-            {/*                </div>*/}
-            {/*            </Nav>*/}
-            {/*        </Navbar.Collapse>*/}
-            {/*    </Container>*/}
-            {/*</Navbar>*/}
-            <br/>
-            <nav className="navbar navbar-expand-sm justify-content-center align-items-start">
-                <button className="navbar-toggler mr-auto" type="button" data-toggle="collapse" data-target="#navbar">
+            <nav className="navbar navbar-expand-sm  navbar-light flex-nowrap">
+                <button className="navbar-toggler mr-2" type="button" data-toggle="collapse" data-target="#navbar5">
                     <span className="navbar-toggler-icon"></span>
                 </button>
-                <div className="navbar-brand position-absolute mx-0">
-                    <ul className="navbar-nav justify-content-center">
+                <span className="navbar-brand w-100">
+                    <Link to={'/'}><img src={logo} style={{height: 80}} alt="Logo"/></Link>
+                </span>
+                <div className="navbar-collapse collapse w-100 justify-content-center" id="navbar5">
+                    <ul className="navbar-nav mx-auto ">
                         <li>
-                            <Link className="nav-link d-flex align-items-center" to={'/visit'}>
-                                <img className="float-start img-fluid mx-2" src={visit} style={{height: 80}}
+                            <Link className="nav-link d-flex align-items-center fs-5 px-5" to={'/visit'}>
+                                <img className="float-start img-fluid mx-2" src={visit} style={{height: 60}}
                                      alt="visit"/>
-                                <p>VISIT <br/>HISTORY</p>
+                                <p>VISIT HISTORY</p>
                             </Link>
                         </li>
 
                         <li>
-                            <Link className="nav-link d-flex align-items-center" to={'#'}>
-                            <img className="float-start img-fluid mx-2" src={drugs} style={{height: 80}}
-                                 alt="drugs"/>
-                            <p>DRUGS</p>
-                        </Link>
+                            <Link className="nav-link d-flex align-items-center fs-5 px-5" to={'#'}>
+                                <img className="float-start img-fluid mx-2 " src={drugs} style={{height: 60}}
+                                     alt="drugs"/>
+                                <p>DRUGS</p>
+                            </Link>
                         </li>
 
                         <li>
-                            <Link className="nav-link d-flex align-items-center" to={'#'}>
-                                <img className="float-start img-fluid mx-2" src={contact} style={{height: 80}}
+                            <Link className="nav-link d-flex align-items-center fs-5 px-5" to={'#'}>
+                                <img className="float-start img-fluid mx-2" src={contact} style={{height: 60}}
                                      alt="contact"/>
                                 <p>CONTACT</p>
                             </Link>
                         </li>
 
                         <li>
-                            <Link className="nav-link d-flex align-items-center" to={'/register-visit'}>
-                                <img className="float-start img-fluid mx-2" src={newVisit} style={{height: 80}}
+                            <Link className="nav-link d-flex align-items-center fs-5 px-5" to={'/register-visit'}>
+                                <img className="float-start img-fluid mx-2" src={newVisit} style={{height: 60}}
                                      alt="newVisit"/>
                                 <p>MAKE AN <br/>APPOINTMENT</p>
                             </Link>
                         </li>
-
-
-
                     </ul>
+                </div>
+                <div className="w-100">
+                    <div className="nav-link d-flex justify-content-end pb-5">
+                        <div className="button-login  fs-3 text-black border border-dark border-2 rounded-pill p-2 "
+                             role="button">
+                            <p>Login | Register </p>
+                            <div className="fs-1">
+                                <CgKey/>
+                                {/*<img src={key} alt="key"/>*/}
 
-                </div>
-                <div className="navbar-collapse collapse" id="navbar">
-                    <div className="navbar-nav justify-content-center">
-                        <div className="navbar-brand">
-                            <Link to={'/'}><img src={logo} style={{height: 80}} alt="Logo"/></Link>
-                        </div>
-                    </div>
-                </div>
-                <div className="d-flex justify-content-end pb-5">
-                    <div className="button-login  fs-3 text-black border border-dark border-2 rounded-pill p-2 "
-                         role="button">
-                        <p>Login | Register </p>
-                        <div className="fs-1">
-                            <CgKey/>
-                            {/*<img src={key} alt="key"/>*/}
+                            </div>
 
                         </div>
-
                     </div>
                 </div>
             </nav>
 
-<br />
-
-
-            {/*<nav className="navbar navbar-expand-lg bg-light justify-content-center align-items-start">*/}
-            {/*    <div className="container-fluid justify-content-between">*/}
-
-            {/*        <button className="navbar-toggler" type="button" data-bs-toggle="collapse"*/}
-            {/*                data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup"*/}
-            {/*                aria-expanded="false"*/}
-            {/*                aria-label="Toggle navigation">*/}
-            {/*            <span className="navbar-toggler-icon"></span>*/}
-            {/*        </button>*/}
-            {/*        <div className="collapse navbar-collapse d-flex justify-content-center " id="navbarNavAltMarkup">*/}
-            {/*            <div className="navbar-brand position-absolute mx-0">*/}
-            {/*                <ul className="navbar-nav justify-content-center">*/}
-            {/*                    <li>*/}
-            {/*                        <Link className="nav-link d-flex align-items-center" to={'/visit'}>*/}
-            {/*                            <img className="float-start img-fluid mx-2" src={visit} style={{height: 80}}*/}
-            {/*                                 alt="visit"/>*/}
-            {/*                            <p>VISIT HISTORY</p>*/}
-            {/*                        </Link>*/}
-            {/*                    </li>*/}
-
-            {/*                    <li>*/}
-            {/*                        <Link className="nav-link d-flex align-items-center" to={'#'}>*/}
-            {/*                            <img className="float-start img-fluid mx-2" src={drugs} style={{height: 80}}*/}
-            {/*                                 alt="drugs"/>*/}
-            {/*                            <p>DRUGS</p>*/}
-            {/*                        </Link>*/}
-            {/*                    </li>*/}
-
-            {/*                    <li>*/}
-            {/*                        <Link className="nav-link d-flex align-items-center" to={'#'}>*/}
-            {/*                            <img className="float-start img-fluid mx-2" src={contact} style={{height: 80}}*/}
-            {/*                                 alt="contact"/>*/}
-            {/*                            <p>CONTACT</p>*/}
-            {/*                        </Link>*/}
-            {/*                    </li>*/}
-
-            {/*                    <li>*/}
-            {/*                        <Link className="nav-link d-flex align-items-center" to={'/register-visit'}>*/}
-            {/*                            <img className="float-start img-fluid mx-2" src={newVisit} style={{height: 80}}*/}
-            {/*                                 alt="newVisit"/>*/}
-            {/*                            <p>MAKE AN <br/>APPOINTMENT</p>*/}
-            {/*                        </Link>*/}
-            {/*                    </li>*/}
-            {/*                </ul>*/}
-            {/*            </div>*/}
-            {/*        </div>*/}
-            {/*        <div className="d-flex justify-content-end pb-5">*/}
-            {/*            <div className="button-login  fs-3 text-black border border-dark border-2 rounded-pill p-2 "*/}
-            {/*                 role="button">*/}
-            {/*                <p>Login | Register </p>*/}
-            {/*                <div className="fs-1">*/}
-            {/*                    <CgKey/>*/}
-            {/*                    /!*<img src={key} alt="key"/>*!/*/}
-
-            {/*                </div>*/}
-
-            {/*            </div>*/}
-            {/*        </div>*/}
-
-
-            {/*    </div>*/}
-
-            {/*</nav>*/}
         </>
     );
 
