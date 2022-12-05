0f11fe8f504bd3dc8be822411a80a9937bceecb9
szopszop <szymontracz1@gmail.com>
2022-12-05 15:22:17 +0100

    Changes by ochot

--- src/components/Navbar/TheNavbar.js
+++ src/components/Navbar/TheNavbar.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, {useState, useEffect} from "react"
 import logo from '../../assets/logo/logo.svg'
 import visit from '../../assets/icons/Visit.svg'
 import drugs from '../../assets/icons/Drugs.svg'
@@ -9,11 +9,16 @@ import {Link} from "react-router-dom";
 import {CgKey} from "react-icons/cg"
 
 export default function TheNavbar() {
+
+
     return (
         <>
             <nav className="navbar navbar-expand-md bg-light navbar-light flex-sm-nowrap flex-wrap">
                 <div className="container-fluid">
-                    <button className="navbar-toggler flex-grow-sm-1 flex-grow-0 me-2" type="button" data-bs-toggle="collapse" data-bs-target="#navbar5">
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
+                            data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup"
+                            aria-expanded="false"
+                            aria-label="Toggle navigation">
                         <div className="border border-dark border-2 rounded-pill ">
                             <img className="" src={hamburger} style={{height: 60}}
                                  alt="hamburger"/>
@@ -65,13 +70,13 @@ export default function TheNavbar() {
                     </div>
                     <div className="w-100">
                         <div className="nav-link d-flex justify-content-end pb-5">
-                            <div className="button-login  fs-3 text-black border border-dark border-2 rounded-pill p-2 "
-                                 role="button">
-                                <p>Login | Register </p>
-                                <div className="fs-1">
+                            <div
+                                className="menu-login button-login fs-3 text-black border border-dark border-2 rounded-pill p-2 "
+                                role="button">
+                                <p>{'\u00A0'}{'\u00A0'}Login | Register{'\u00A0'}</p>
+                                <div className="fs-1 d-inline">
                                     <CgKey/>
                                 </div>
-
                             </div>
                         </div>
                     </div>
