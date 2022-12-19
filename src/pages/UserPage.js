fd3824dcff63365c9dc0c8a39496b7601565179b
szopszop <szymontracz1@gmail.com>
2022-12-19 07:31:42 +0100

    user pages beginning

--- src/pages/UserPage.js
+++ src/pages/UserPage.js
@@ -4,7 +4,18 @@ export default function UserPage() {
 
     return (
         <>
-            USER PAGE
+            <div className="container-fluid col-lg-6 col-md-12 col-sm-12
+             mx-auto rounded-5 bg-light text-dark  mt-3 green-shadow">
+                <div className="row justify-content-center">
+                    <div className="col">USER PAGE</div>
+                    <div className="col">
+
+                        {/*<TheCalendar />*/}
+
+                    </div>
+                </div>
+            </div>
+
         </>
     )
 }
