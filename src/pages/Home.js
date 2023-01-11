5ca854080984a18b46f1697f63db30c86f1ac42c
szopszop <szymontracz1@gmail.com>
2023-01-11 17:03:58 +0100

    homepage is a mess

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,36 +1,59 @@
-import React from "react"
+import React, {useEffect} from "react"
 import logo from '../assets/logo/logo1.svg'
 import News from "../components/NewsCarousel/News";
 import Footer from "../components/Footer/Footer";
 import {useAuth} from "../components/Auth/Auth";
-
-const observer = new IntersectionObserver((entries) => {
+import name_logo from "../assets/logo/name.png"
+import bg_doc from "../assets/background/bg_doctor.png"
+import './Home.css';
+let observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
         console.log(entry)
         if (entry.isIntersecting) {
+            console.log("ENTRY")
             entry.target.classList.add('showX');
         } else {
             entry.target.classList.remove('showX');
+            console.log("ERLO")
         }
     })
 })
 
+
 const hiddenElements = document.querySelectorAll('.hidden')
 hiddenElements.forEach((el) => observer.observe(el));
+console.log(hiddenElements + "hidden")
+
 
 
 export default function Home() {
     const auth = useAuth()
     console.log(auth.email)
+
+    useEffect(() => {
+        const hiddenElements = document.querySelectorAll('.hidden')
+        hiddenElements.forEach((el) => observer.observe(el));
+    })
     return (
         <div>
-
+            <img className="" src={bg_doc}
+                 alt="logo" style={{position: "absolute"}}/>
 
             {auth.role !== "PATIENT" ?
                 <div >
                     <section className="hidden">
+
+                        <div className="container">
+                            <div className="row d-flex justify-content-center">
+                                <div className="col-auto">
+                                    <img className="" src={name_logo}
+                                         alt="logo" style={{height: 160}}/>
+
+                                </div>
+                            </div>
+                        </div>
                         <div className="fs-1 px-4">Welcome to App Sick</div>
-                        <div className="fs-3">With our help you can make an appointment quickly and safely.<br/>
+                        <div className="fs-3 text-center">With our help you can make an appointment quickly and safely.<br/>
                             Give it a try now.
                             <br/>
                             {/*<div className="article">*/}
@@ -42,25 +65,25 @@ export default function Home() {
                     </section>
                     <section className="hidden">
 
-                        <div className="fs-1">
-                            AppSick is a promising startup in the private health sector in Poland. Our ambition is to
-                            deliver the most accessible and convenient systems for private healthcare market.
+                        <div className="fs-1 text-center">
+                            AppSick is a promising startup in the private health sector in Poland.<br/>
+                            Our ambition is to deliver the most accessible and convenient systems <br/>
+                            for private healthcare market.
                         </div>
                     </section>
                     <section className="hidden">
-                        <div className="fs-1">
-                            This means a commitment to excellent customer experience, through great service and value.
+                        <div className="fs-1" text-center>
+                            This means a commitment to excellent customer experience,<br/> through great service and value.
                         </div>
                     </section>
                     <section className="hidden">
-                        <div className="fs-1">
+                        <div className="fs-1 text-center">
                             Our application is aimed at coordination of private healthcare companies.
                         </div>
                     </section>
                     <section className="hidden">
-                        <div className="fs-1">
-                            We offer management systems for both digital & local consultations between a patient and a
-                            doctor.
+                        <div className="fs-1 text-center">
+                            We offer management systems for both digital & local <br />consultations between a patient and a doctor.
                         </div>
                     </section>
 
