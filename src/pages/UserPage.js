962998c2191f70214bbc259bbea116824373e2a2
Sebastian Dudkowski <sebastian.dudkowski@gmail.com>
2023-01-12 21:05:56 +0100

    fetch patient data

--- src/pages/UserPage.js
+++ src/pages/UserPage.js
@@ -8,10 +8,22 @@ export default function UserPage() {
 
     const dispatch = useDispatch();
     const history = useHistory();
-    const [userId, setUserId] = useState(null);
+    const [user, setUser] = useState(null);
+    const [patient, setPatient] = useState(null);
 
     async function getUser() {
         const data = await fetch("http://localhost:8080/api/auth/current", {
+            credentials: "include"
+        })
+        try {
+            return await data.json()
+        } catch (e) {
+            console.warn(e.message)
+            return data;
+        }
+
+    }async function getPatient(userId) {
+        const data = await fetch(`http://localhost:8080/api/patient/${userId}?user_id=true`, {
             credentials: "include"
         })
         try {
@@ -26,16 +38,29 @@ export default function UserPage() {
         getUser()
             .then(user => {
                 if (user?.id) {
-                    setUserId(user.id);
+                    setUser(user);
                 } else {
                     setTimeout(() => {history.push("/")}, 3000);
                     dispatch(showModal());
                 }
             })
 
-    }, [userId]);
+    }, []);
 
-    if (!userId){
+    useEffect(() => {
+        if (!user?.id) { return; }
+        getPatient(user.id)
+            .then(patient => {
+                if (patient?.patientId) {
+                    setPatient(patient);
+                } else {
+                    console.warn("Error while fetching Patient by User ID")
+                }
+            })
+
+    }, [user]);
+
+    if (!user){
         return (
             <div className={"container fixed-top mx-auto m-5 text-center"}>
                 Redirecting... <Spinner size={'md'}></Spinner>
